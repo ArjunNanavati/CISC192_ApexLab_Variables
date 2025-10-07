938a3ce48b514ecbb96ee79a5197b3dbeb26e0ed
ArjunNanavati <tootsie.nanavati@gmail.com>
2025-10-07 13:24:46 -0700

    apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -7,24 +7,29 @@
 using namespace std;
 
 void minutes_to_12hour_time(stringstream &cin, stringstream &cout) {
-    // Place code for translating minutes to 12 hour time
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
-
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
-
-    // First you want to get user input with cin
-    // Do any needed calculations
-    // Create output with cout
+    int totalMinutes;
+        cin >> totalMinutes;
+        int hours = (totalMinutes / 60) % 12;
+        int minutes = totalMinutes % 60;
+        cout << hours << ":" << minutes << " !" << endl;
 }
 
-void numbers_to_12hour_time(stringstream& cin, stringstream& cout)
-{
-    // Place code for translating hours, minutes and seconds to 12 hour time
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
+void numbers_to_12hour_time(stringstream& cin, stringstream& cout) {
+  
+    double totalHours;
+    int totalMinutes;
+    int totalSeconds;
 
-    // If you want to print to the console for debugging use std::cout.
-    // Use cout for your final solution.
+    cin >> totalHours;
+    cin >> totalMinutes;
+    cin >> totalSeconds;
+
+    int totalSecondsAll = (totalHours * 3600) + (totalMinutes * 60) + totalSeconds;
+
+    int hours = (totalSecondsAll / 3600) % 12;       
+    int minutes = (totalSecondsAll / 60) % 60;    
+    int seconds = totalSecondsAll % 60;             
+
+    cout << hours << ":" << minutes << "." << seconds << " !" << endl;
 }
+
